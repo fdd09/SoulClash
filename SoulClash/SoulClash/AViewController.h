509957d95416bbab2a81e6a27f82f967ab8ad21a
fengdongwang <fengdongwang@aikucun.com>
2020-07-29 16:30:27 +0800

    a1

---
+++ SoulClash/SoulClash/AViewController.h
@@ -0,0 +1,17 @@
+//
+//  AViewController.h
+//  SoulClash
+//
+//  Created by fengdongwang on 2020/7/29.
+//  Copyright © 2020 冯东旺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SoulClash/SoulClash/AViewController.m
@@ -0,0 +1,32 @@
+//
+//  AViewController.m
+//  SoulClash
+//
+//  Created by fengdongwang on 2020/7/29.
+//  Copyright © 2020 冯东旺. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
