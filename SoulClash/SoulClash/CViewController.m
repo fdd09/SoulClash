e04fcc99e37c205f5f5cf306d0634fda73960ce7
fengdongwang <fengdongwang@aikucun.com>
2020-07-29 17:28:33 +0800

    c1

---
+++ SoulClash/SoulClash/CViewController.m
@@ -0,0 +1,33 @@
+//
+//  CViewController.m
+//  SoulClash
+//
+//  Created by fengdongwang on 2020/7/29.
+//  Copyright © 2020 冯东旺. All rights reserved.
+//
+
+#import "CViewController.h"
+
+@interface CViewController ()
+
+@end
+
+@implementation CViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    //c1
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
