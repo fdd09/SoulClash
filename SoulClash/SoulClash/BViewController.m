c3b779045263ec378f077f0a034681f1300a9e4f
fengdongwang <fengdongwang@aikucun.com>
2020-07-29 16:29:51 +0800

    b1

---
+++ SoulClash/SoulClash/BViewController.m
@@ -0,0 +1,32 @@
+//
+//  BViewController.m
+//  SoulClash
+//
+//  Created by fengdongwang on 2020/7/29.
+//  Copyright © 2020 冯东旺. All rights reserved.
+//
+
+#import "BViewController.h"
+
+@interface BViewController ()
+
+@end
+
+@implementation BViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
